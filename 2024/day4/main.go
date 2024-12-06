08e85dfdfc113cbe07e022137ab7993016eb4d32
boatnoah <noahskim7@gmail.com>
2024-12-06 02:36:24 -0800

    2024: day4 solution complete **

--- 2024/day4/main.go
+++ 2024/day4/main.go
@@ -219,15 +219,6 @@ func freqArray(s string) map[rune]int {
 	return freq
 }
 
-func reverse(s string) string {
-	rns := []rune(s)
-	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
-		rns[i], rns[j] = rns[j], rns[i]
-	}
-
-	return string(rns)
-}
-
 func main() {
 	var input string
 	fmt.Print("Show Part 1 or 2: ")
